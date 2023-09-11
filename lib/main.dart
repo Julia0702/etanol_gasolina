07b5f8f5a2a2584e7095bed0171d332f503a6c03
julia <julia.alves79@etec.sp.gov.br>
2023-09-11 10:13:49 -0300

    arquivo enviado

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'home.dart';
+
+void main(List<String> args) {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
